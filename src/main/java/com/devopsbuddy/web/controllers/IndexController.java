13821f8a77047dfc1c425d8e7fdd7819793a04c1
sajedeh <sajedehharatinike@gmail.com>
2017-07-07 14:27:46 +0000

    logging method

--- src/main/java/com/devopsbuddy/web/controllers/IndexController.java
+++ src/main/java/com/devopsbuddy/web/controllers/IndexController.java
@@ -1,5 +1,7 @@
 package com.devopsbuddy.web.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,8 +10,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @Controller
 public class IndexController {
+
+    /* The application logger*/
+    private static final Logger LOG = LoggerFactory.getLogger(IndexController.class);
     @RequestMapping("/")
     public String home(){
+        LOG.info("home()");
         return "index";
     }
 }
